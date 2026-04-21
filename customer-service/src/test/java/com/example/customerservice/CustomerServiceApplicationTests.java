f4679b8948f28f98a8754033197b2bf112d6ace6
JannoLawendy <jannohany24@yahoo.com>
2026-04-21 16:45:26 -0400

    mychanges

--- customer-service/src/test/java/com/example/customerservice/CustomerServiceApplicationTests.java
+++ customer-service/src/test/java/com/example/customerservice/CustomerServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.carrentalmicroservices;
+package com.example.customerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarRentalMicroservicesApplicationTests {
+class CustomerServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
--- vehicle-service/src/test/java/com/example/vehicleservice/VehicleServiceApplicationTests.java
+++ vehicle-service/src/test/java/com/example/vehicleservice/VehicleServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.carrentalmicroservices;
+package com.example.vehicleservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarRentalMicroservicesApplicationTests {
+class VehicleServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
