6d8cab7238c81439a78d64b94d88891efaa8fe6a
JannoLawendy <jannohany24@yahoo.com>
2026-04-21 16:17:45 -0400

    Initial microservices project setup

---
+++ src/test/java/com/example/carrentalmicroservices/CarRentalMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentalmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
